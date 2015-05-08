30ece8a0fdd7cf17785032e08f1e8ed3049d3415
Keith Brister <k-brister@northwestern.edu>
2015-05-08 18:50:12 +0000

    G startup
    git-svn-id: file:///contrabass/repos/svn/epics/trunk@3979 66ca1eeb-be3a-49d5-93d8-8bdade05efff

---
+++ lsredis/iocBoot/iocredis1/st-g.cmd
@@ -0,0 +1,17 @@
+#!../../bin/linux-x86_64/redis
+
+## You may have to change redis to something else
+## everywhere it appears in this file
+
+#< envPaths
+epicsEnvSet("TOP","/usr/local/epics/synApps_5_7/support/lsredis")
+
+## Register all support components
+dbLoadDatabase("$(TOP)/dbd/redis.dbd",0,0)
+redis_registerRecordDeviceDriver(pdbbase) 
+
+
+dbLoadRecords("$(TOP)/db/redis.db","P=21:mango,S=stns.4,RRH=localhost,RRP=6379,RRDB=0,RWH=mango-2.ls-cat.org,RWP=6379,RWDB=0,pgHost=10.1.0.3,pgPort=5432,pgUser=lsuser,pgDb=ls")
+
+
+iocInit()
