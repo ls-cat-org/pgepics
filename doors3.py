cea4913b2934e1d419f5d9aa39545c0ab9b2c17d
Keith Brister <k-brister@northwestern.edu>
2020-11-02 10:43:10 -0600

    Changed db connector, added detector lower limit check

--- doors3.py
+++ doors3.py
@@ -3,17 +3,74 @@
 # Copyright 2016 by Northwestern University
 #
 import epics
-import postgresql
+import pg
 import time
 
 open_fe_shutter = "PC:21ID:FES_OPEN_REQUEST"
+
+class _Q:
+    
+    db = None   # our database connection
+
+    def open( self):
+        self.db = pg.connect( dbname="ls", host="contrabass.ls-cat.org", user="lsuser" )
+
+    def close( self):
+        self.db.close()
+
+    def __init__( self):
+        self.open()
+
+    def reset( self):
+        self.db.reset()
+
+    def query( self, qs):
+        if qs == '':
+            return rtn
+        if self.db.status == 0:
+            self.reset()
+        try:
+            # ping the server
+            qr = self.db.query(qs)
+        except:
+            if self.db.status == 1:
+                print(sys.exc_info()[0], file=sys.stderr)
+                print('-'*60, file=sys.stderr)
+                traceback.print_exc(file=sys.stderr)
+                print('-'*60, file=sys.stderr)
+                return None
+            # reset the connection, should
+            # put in logic here to deal with transactions
+            # as transactions are rolled back
+            #
+            self.db.reset()
+            if self.db.status != 1:
+                # Bad status even after a reset, bail
+                raise PVServiceError( 'Database Connection Lost')
+
+            qr = self.db.query( qs)
+
+        return qr
+
+    def dictresult( self, qr):
+        return qr.dictresult()
+
+    def e( self, s):
+        return pg.escape_string( s)
+
+    def fileno( self):
+        return self.db.fileno()
+
+    def getnotify( self):
+        return self.db.getnotify()
+
 
 class RemoteControl:
     MOVE_TO_DIST = 700.0                # move back to here
     IGNORE_DIST  = 500.0                # don't move unless less than this
     MINIMUM_DS_UNDULATOR_GAP = 21       # Close the shutter if the side station is below this number
 
-    def __init__( self, pvd, db):
+    def __init__( self, pvd, q):
         #
         # Our state.  Initial it's unknown so we won't be doing much
         # of anything at first.
@@ -43,9 +100,12 @@ class RemoteControl:
         self.shutterPV          = None          # PV object for the station shutter state
         self.voicePV            = None          # PV object for the search voice state
         self.waitingForCover    = False         # True when we are waiting for the cover to open before moving the detector back
+        self.station            = None          # Name of our station
 
         self.pvd = pvd
-        self.db = db
+        self.q = q
+
+        self.station = pvd["station"]
 
         #
         # Initialize PV objects
@@ -61,6 +121,11 @@ class RemoteControl:
         self.lastDoor = self.doorPV.value
         self.doorPV.add_callback( callback=self.doorCB)
 
+        if "detectorLowerLimit" in pvd:
+            self.detectorLLPV = epics.PV( pvd["detectorLowerLimit"], callback=self.detectorLLCB)
+            self.detectorLLPV.value
+            self.add_callback( callback=self.detectorLLCB)
+
         self.readyAPV = epics.PV( pvd["readyA"])
         self.readyAPV.value
         self.readyAPV.add_callback( callback=self.readyCB)
@@ -72,16 +137,16 @@ class RemoteControl:
         #
         # Currently only the Eiger detectors have covers
         #
-        if pvd.has_key("cover"):
+        if "cover" in pvd:
             self.coverPV = epics.PV(pvd["cover"])
             self.coverPV.value
 
-        if pvd.has_key("coverOpened"):
+        if "coverOpened" in pvd:
             self.coverOpenedPV    = epics.PV( pvd["coverOpened"]);
             self.coverOpenedSaved = self.coverOpenedPV.value
             self.coverOpenedPV.add_callback(callback=self.coverOpenedCB);
 
-        if pvd.has_key("coverClosed"):
+        if "coverClosed" in pvd:
             self.coverClosedPV = epics.PV( pvd["coverClosed"])
             self.coverClosedPV.value
 
@@ -94,8 +159,38 @@ class RemoteControl:
         # database calls.
         #
         self.detectorName = pvd["detector"]
-        qr = self.db.prepare( "select epics.position( '%s') as p" % (self.detectorName))
-        self.detectorSaved = float(qr.first())
+
+        qr = self.q.query( "select epics.position( '%s') as p" % (self.detectorName))
+        self.detectorSaved = float(qr.dictresult()[0]["p"])
+
+    #
+    # Called when the detector lower limit has changed.  We need this
+    # since just after the cover has opened the lower limit has not
+    # yet changed and, hence, the move might fail.
+    #
+    def detectorLLCB(self, **kw):
+        value = float(kw["value"])
+
+        if not self.waitingForCover:
+            return
+        
+        self.waitingForCover = False
+        #
+        # I suppose it's possible that we are waiting for the cover
+        # but the lower limit is still higher than the position we'd
+        # like to move the detector.  Hopefully this is a weird and
+        # rare event.  Best just to reset the waitingForCover flag and
+        # leave without trying to move the detector.
+        #
+        if self.detectorSaved < value:
+            print( "    Waiting for cover by detector lower limit %f still high than desired position %f" % (value, self.detectorSaved))
+            return;
+
+        qs = "select epics.moveit( '%s', %f)" % (self.detectorName, self.detectorSaved)
+        print( "    query: %s" % (qs))
+        self.q.query(qs)
+
+
 
     #
     # Called when the cover open switch changes state.  Used to move
@@ -109,18 +204,26 @@ class RemoteControl:
         else:
             strv = "Opened"
 
-        print("[%s %s] lidOpenCB value=%d %s" % (self.station, time.strftime('%Y-%m-%d %H:%M:%S'), value, strv))
+        print("[%s %s] coverOpenCB value=%d %s" % (self.station, time.strftime('%Y-%m-%d %H:%M:%S'), value, strv))
 
         if value == 0 and self.waitingForCover:
+            if self.detectorSaved < self.detectorLLPV.value:
+                #
+                # Moving the detector now will fail.  Wait for the
+                # lower limit to changed before trying to move the
+                # detector.
+                #
+                return
+            
             qs = "select epics.moveit( '%s', %f)" % (self.detectorName, self.detectorSaved)
-            print "    query: %s" % (qs)
-            self.db.prepare( qs).first()
+            print( "    query: %s" % (qs))
+            self.q.query(qs)
 
         #
         # Regardless of what happens above we are not waiting for the cover now.
         #
         # When the cover just changed from opened to not opened we
-        # clearly are doing something that does not envovle us moving
+        # clearly are doing something that does not envolve us moving
         # the detector.  When the cover just changed from not opened
         # to opened while waiting for the detector then we just moved
         # the detector a couple of lines ago and now we're done with
@@ -166,8 +269,8 @@ class RemoteControl:
             #
             # Save our current detector position so we might return to it later
             #
-            qr = self.db.prepare( "select epics.position( '%s') as p" % (self.detectorName))
-            self.detectorSaved = float(qr.first())
+            qr = self.q.query( "select epics.position( '%s') as p" % (self.detectorName))
+            self.detectorSaved = float(qr.dictresult()[0]["p"])
 
             #
             # Save cover state
@@ -182,7 +285,7 @@ class RemoteControl:
             # Don't move the detector back if it's fine where it is
             #
             if self.detectorSaved < self.IGNORE_DIST:
-                self.db.prepare( "select epics.moveit( '%s', %f)" % (self.detectorName, self.MOVE_TO_DIST)).first()
+                self.q.query( "select epics.moveit( '%s', %f)" % (self.detectorName, self.MOVE_TO_DIST))
 
                 print('   doorCB moveit %f' % (self.MOVE_TO_DIST))
 
@@ -216,6 +319,7 @@ class RemoteControl:
                 # Only wait for the cover if we are changing its
                 # state.
                 #
+                self.waitingForCover = False
                 if self.coverOpenedPV.value != self.coverOpenedSaved and self.detectorSaved < self.IGNORE_DIST:
                     self.waitingForCover = True
 
@@ -223,7 +327,7 @@ class RemoteControl:
 
                 qs = "select epics.moveit( '%s', %f)" % (self.detectorName, self.detectorSaved)
                 print("    query: %s" % (qs))
-                self.db.prepare( qs).first()
+                self.q.query(qs)
 
         self.lastDoor = value
 
@@ -232,7 +336,7 @@ class RemoteControl:
     #
     def readyCB( self, pv=None, **kw):
 
-        print( "[%s %s]  Chain A Ready: %d  Chain B Reading: %d" % (self.station,time.strftime('%Y-%m-%d %H:%M:%S'), self.readyPV.value, self.readyPV.value))
+        print( "[%s %s]  Chain A Ready: %d  Chain B Reading: %d" % (self.station,time.strftime('%Y-%m-%d %H:%M:%S'), self.readyAPV.value, self.readyBPV.value))
 
         if self.readyAPV.value == 1 and self.readyBPV.value == 1 and (self.lastReadyA == 0 or self.lastReadyB == 0):
             self.shutterPV.value = 1
@@ -243,7 +347,6 @@ class RemoteControl:
 
 
 class Doors:
-    db = None
     pvN = [
         { 'station' : '21-ID-D', 'voice' : 'PA:21ID:D_VOICE_SEARCHING',    'shutter' : 'PC:21ID:SDS_OPEN_REQUEST',
           'door' : 'PA:21ID:D_DOOR_2_CLOSED',    'detector' : '21:D1:DT:Z0', 'readyA' : 'PA:21ID:SDS_PERMIT', 'readyB' : 'PB:21ID:SDS_PERMIT',
@@ -263,10 +366,11 @@ class Doors:
     rcL = {}
 
     def __init__( self):
-        self.db       = postgresql.open("pq://lsuser@10.1.0.3/ls")
+        self.q = _Q()
+
         for pvs in self.pvN:
-            print pvs
-            self.rcL[pvs["station"]] = RemoteControl( pvs, self.db)
+            print( pvs)
+            self.rcL[pvs["station"]] = RemoteControl( pvs, self.q)
 
     def run( self):
         while 1:
